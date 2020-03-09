156e0218073ac471788e1ccf046cbfea65580e81
bayuswara <54299369+bayuswara@users.noreply.github.com>
2020-03-09 09:52:14 +0700

    Add files via upload

---
+++ Latihan2.php
@@ -0,0 +1,8 @@
+<?php
+$nilai = 75;
+if ($nilai >= 75) {
+    echo "Nilai anda $nilai, Anda Lulus";
+} else {
+    echo "Nilai Anda $nilai, Anda Gagal";
+}
+?>
---
+++ latihan1.php
@@ -0,0 +1,17 @@
+<?php 
+
+// membuat fungsi 
+function perkenalan() {
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Ardi<br/>";
+    echo "Senang Berkenalan denganmu<br/>";
+}
+
+// Memanggil fungsi yang sudah dibuat 
+perkenalan();
+
+echo "<hr>";
+
+//memanggilnya lagi
+perkenalan();
+?>
